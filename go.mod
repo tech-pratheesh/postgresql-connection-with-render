9ed37454d1a44e5ca6456b5bc08b455d352c9998
tech-pratheesh <pratheesh@techversantinfo.com>
2023-07-10 10:58:51 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tech-pratheesh/postgresql-connection-with-render
+
+go 1.20
+
+require github.com/lib/pq v1.10.9
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	host := os.Getenv("PSQL_DB_HOST")
+	port := os.Getenv("PSQL_DB_PORT")
+	user := os.Getenv("PSQL_DB_USER")
+	pass := os.Getenv("PSQL_DB_PASS")
+	database := os.Getenv("PSQL_DB_DATABASE")
+	sslmode := os.Getenv("PSQL_DB_SCHEMA")
+
+	// Set up a connection string with the database credentials
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%v sslmode=%s", user, pass, database, host, port, sslmode)
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to PostgreSQL database!")
+}
